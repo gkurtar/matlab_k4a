6d11329b442990a773e8339475f689a10bd700f7
gkurtar <gultekinkurtar@gmail.com>
2022-06-08 15:32:11 +0300

    Update fun_calibrate_depth_data.m

--- fun_calibrate_depth_data.m
+++ fun_calibrate_depth_data.m
@@ -1,4 +1,4 @@
-function [ correctedImage ] = fun_calibrate_depth_data(input_file_name, rows, cols)
+function [ correctedImage ] = fun_calibrate_depth_data(cameraParams, rows, cols)
 
 	fprintf("\nBEGIN: fun_calibrate_depth_data");
 
@@ -14,34 +14,41 @@ function [ correctedImage ] = fun_calibrate_depth_data(input_file_name, rows, co
         for j=1:cols
             rowIndex = (i - 1) * cols + j;
             %dimg_mat(i, j) = 0;
-            if (j < 20) 
+            if (j < 20)
                 fprintf ("i %d, j %d, row %d depth %d \n", i, j, rowIndex, depthData(rowIndex, colIndex));
             end
             dimg_mat(i, j) = depthData(rowIndex, colIndex);
-
         end
     end
 
+    figure;
+    title('Original');
     result_img = imshow(dimg_mat, []);
     figure;
 
     size(result_img.CData)
     
+    fprintf("\nresult_img %s %d", ...
+        result_img.Type, result_img.CData(12, 34));
 
-    fprintf("\nresult_img %s %s %d", ...
-        size(result_img), result_img.Type, result_img.CData(12, 34));
+    J1 = undistortImage(result_img.CData, cameraParams);
+    UD_DEPTH_IMAGE = imshow(J1);
+    title('Corrected');
 
+    fprintf("\nresult_img %s %d", ...
+        UD_DEPTH_IMAGE.Type, UD_DEPTH_IMAGE.CData(12, 34));
+
+    fileID = fopen('ud_depth.txt', 'w');
     for i=1:rows
         for j=1:cols
- 
-            if (j > 120 && j < 240 && i < 320 && i > 180) 
-               dimg_mat(i, j) = dimg_mat(i, j) * 1.3;
-            end
-            
+            %fprintf(fileID, "%d %d %d\n", i, j, UD_DEPTH_IMAGE.CData(i, j));
+            fprintf(fileID, "%d %d %d\n", i, j, cast(UD_DEPTH_IMAGE.CData(i, j), "uint16"));
         end
     end
+    %imshow(dimg_mat, []);
 
-    imshow(dimg_mat, []);
+    fclose(fileID);
+    fprintf("\ngenerated undistorted depth data");
 
     fprintf("\nEND: fun_calibrate_depth_data");
 	return;
