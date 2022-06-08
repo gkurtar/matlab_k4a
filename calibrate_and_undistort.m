f315286b022a2972566a8129f886bf8c2cea20b4
gkurtar <gultekinkurtar@gmail.com>
2022-06-08 15:24:57 +0300

    Update calibrate_and_undistort.m

--- calibrate_and_undistort.m
+++ calibrate_and_undistort.m
@@ -1,22 +1,34 @@
-images = imageDatastore({'image1.gif', 'image2.gif', 'image3.gif', 'image4.gif', 'image5.gif'});
+function [ cameraParams ] = fun_detect_camera_params()
 
-%detect calibration pattern
-[imagePoints, boardSize] = detectCheckerboardPoints(images.Files);
+	fprintf("\nBEGIN: fun_detect_camera_params\n");
 
-% Generate world coordinates of the corners of the squares. The square size is in millimeters.
-squareSize = 30;
+    images = imageDatastore({'im1_upd.png', 'im2_upd.png', 'im3_upd.png', 'im4_upd.png'});
+    
+    %detect calibration pattern
+    [imagePoints, boardSize] = detectCheckerboardPoints(images.Files);
+    
+    % Generate world coordinates of the corners of the squares. The square size is in millimeters.
+    squareSize = 30;
+    
+    worldPoints = generateCheckerboardPoints(boardSize, squareSize);
+    
+    % Calibrate the camera.
+    I = readimage(images,1); 
+    imageSize = [size(I,1),size(I,2)];
+    cameraParams = estimateCameraParameters(imagePoints,worldPoints, 'ImageSize',imageSize);
+    
+    %Remove lens distortion and display results
+    
+    I = images.readimage(1);
+    J1 = undistortImage(I,cameraParams);
+    
+    imshow(I);
+    figure;
+    title('Original');
+    imshow(J1);
+    title('Corrected');
+    %figure; imshowpair(I, J1, image);
+    %title('Original vs Corrected');
 
-worldPoints = generateCheckerboardPoints(boardSize, squareSize);
-
-% Calibrate the camera.
-I = readimage(images,1); 
-imageSize = [size(I,1),size(I,2)];
-cameraParams = estimateCameraParameters(imagePoints,worldPoints, 'ImageSize',imageSize);
-
-%Remove lens distortion and display results
-
-I = images.readimage(1);
-J1 = undistortImage(I,cameraParams);
-
-figure; imshowpair(I, J1, image);
-title('Original vs Corrected');
+    fprintf("\nEND: fun_detect_camera_params\n");
+end
