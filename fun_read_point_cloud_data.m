86165ccf8b0199f94d6a669e5e26e8efae3a76d5
gkurtar <gultekinkurtar@gmail.com>
2022-06-16 22:40:01 +0300

    Update fun_read_point_cloud_data.m

--- fun_read_point_cloud_data.m
+++ fun_read_point_cloud_data.m
@@ -1,13 +1,13 @@
 % ***********************************************************
-% detect_errors metod compares two datasets to determine errors
+% fun_read_point_cloud_data
 %
 % % Input Arguments:
-%    argMeasuredDepthValues     -> Measured Sensor Depth Data for a number of points
-%    argRealDepthValues     -> Real Depth Data for corresponding points
+%		argFilePath     -> Path of the file where each line consist of the triplets (row, col, depth)
+%		argRowCount		-> Number of the rows in the depth image
+%		argColCount		-> Number of the columns in the depth image
 %
 % % Output Values:
-%    RMSE -> root mean square error for the datasets as arguments
-%    SDEV ->  standard deviation
+%		matDepthData	-> Depth Data matrix
 %
 % **********************************************************
 
@@ -17,24 +17,28 @@ function [matDepthData] = fun_read_point_cloud_data(argFilePath, argRowCount, ar
 
 	%input control
 	if exist(argFilePath, 'file') ~= 2
-		error("input file does not exist");
+		error(sprintf("input file %s does not exist\n", argFilePath));
 	elseif (~isnumeric(argRowCount) || ~isnumeric(argColCount))
 		error("arguments should be numeric\n");
+	%elseif (~isinteger(int16(argRowCount)) || ~isinteger(int16(argColCount)))
+	%	error("arguments should be integer");
+	elseif (mod(argRowCount, 1) ~= 0 || mod(argColCount, 1) ~= 0)
+		error(sprintf("both %d and %d should be of type integer \n", argRowCount, argColCount));
 	elseif (argRowCount <= 0 || argColCount <= 0)
-		error("Row and Col count arguments should be gt zero \n");
+		error("Row and Col count arguments should be gt zero");
 	end
 
 	tmp = importdata(fullfile(argFilePath));
 	[rows, cols] = size(tmp);
-	fprintf("%s file is imported, sizes are %d %d \n", argFilePath, rows, cols);
-
+	
 	%extra control
 	if (argRowCount * argColCount ~= rows)
 		error("Size of the data file does not match up with row and col count arguments provided");
 	elseif (3 ~= cols)
 		error("Input data file should consist of triplets.\n");
-    end
-
+    	end
+	
+	fprintf("%s file is imported, sizes are %d %d \n", argFilePath, rows, cols);
 	colIndex = 3;
 	matDepthData = zeros(argRowCount, argColCount);
 
@@ -42,12 +46,9 @@ function [matDepthData] = fun_read_point_cloud_data(argFilePath, argRowCount, ar
 		for j = 1 : argColCount
 			rowIndex = (i - 1) * cols + j;
 			matDepthData(i, j) = tmp(rowIndex, colIndex);
-			%if (j == 20)
-			%	fprintf ("i %d, j %d, row %d depth %d \n", i, j, rowIndex, matDepthData(i, j));
-			%end
+			%if (j == 20) fprintf ("i %d, j %d, row %d depth %d \n", i, j, rowIndex, matDepthData(i, j));
 		end
 	end
 
 	fprintf("\nEND: fun_read_point_cloud_data\n");
 end
-
