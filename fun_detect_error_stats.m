f5e48a8e8a06f8253672e67e7cf7ac8c6d007d13
gkurtar <gultekinkurtar@gmail.com>
2022-06-24 19:01:53 +0300

    Update fun_detect_error_stats.m

--- fun_detect_error_stats.m
+++ fun_detect_error_stats.m
@@ -3,18 +3,18 @@
 %
 % % Input Arguments:
 %    argMeasuredDepthValues     -> Measured Sensor Depth Data for a number of points
-%    argRealDepthValues     -> Real Depth Data for corresponding points
+%    argRealDepthValues         -> Real Depth Data for corresponding points
 %
 % % Output Values:
-%    RMSE -> root mean square error for the datasets as arguments
-%    SDEV ->  standard deviation
+%    MAX_ERROR -> max valued error of the diff of the arguments. 
+%    RMSE      -> root mean square errors evaluated for the arguments
+%    SDEV      -> standard deviation of errors evaluated for the arguments
 %
 % **********************************************************
-function [RMSE, SDEV] = fun_detect_error_stats(argMeasuredDepthValues, argRealDepthValues)
+function [MAX_ERROR, RMSE, SDEV] = fun_detect_error_stats(argMeasuredDepthValues, argRealDepthValues)
 
    fprintf("\nBEGIN: fun_detect_error_stats\n");
    
-
    if (~isvector(argMeasuredDepthValues) || ~isvector(argRealDepthValues))
       fprintf('input arguments should be vectors\n');
       return;
@@ -37,19 +37,19 @@ function [RMSE, SDEV] = fun_detect_error_stats(argMeasuredDepthValues, argRealDe
    
    diff_data = argMeasuredDepthValues - argRealDepthValues;
    
-    %compute for real to fitted diff matrix
+   %compute for real to fitted diff matrix
+   MAX_ERROR = max(diff_data);
    SQE = diff_data.^2;
    MSE = mean(SQE(:));
    SDEV = std(diff_data(:));
-   fprintf ("\nDepth Residuals\n\t; mse %f, rmse %f, std_dev: %f", MSE, sqrt(MSE), SDEV);
    RMSE = sqrt(MSE);
+   fprintf ("\nDepth Residuals\n\t; mse %f, rmse %f, std_dev: %f", MSE, RMSE, SDEV);
    
    fprintf("\nMeasured_depth_distances\n");
    fprintf(" %d ", argMeasuredDepthValues.' );
    fprintf("\nReal_depth_distances\n");
    fprintf(" %d ", argRealDepthValues.' );
    
-  
    fprintf("\nEND: fun_detect_error_stats\n");
    return;
 
