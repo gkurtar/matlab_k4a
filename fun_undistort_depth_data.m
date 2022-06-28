c49fa7f4e1e802057d39f20b78bda56e936448eb
gkurtar <gultekinkurtar@gmail.com>
2022-06-28 22:23:57 +0300

    Update fun_undistort_depth_data.m

--- fun_undistort_depth_data.m
+++ fun_undistort_depth_data.m
@@ -1,14 +1,15 @@
 % ***********************************************************
 % 
-% fun_undistort_depth_data function
+% fun_undistort_depth_data
+%
 % K4A depth images are undistorted via IR camera params by this method.
 % Corrected image is returned as an m by n matrix where its sizes are same with the input array.
 %
 % INPUT:
-%   argDepthData	-> Depth Point Cloud as an array imported from a file of triples. The size of this array is [argRowCount * argColCount] [3].
+%   argDepthData	-> a 2D array of size [argRowCount * argColCount] which represents Depth Point Cloud.
 %   argRowCount		-> Row Count of the Depth Data
 %   argColCount		-> Column Count of the Depth Data
-%   argCameraParams 	-> IR(Depth) camera parameters
+%   argCameraParams -> IR(Depth) camera parameters
 %
 % OUTPUT: 
 %   correctedImage	-> corrected image as a two dimensional (depth point cloud) array where its size is eq to [argRowCount] [argColCount]
@@ -20,30 +21,10 @@ function [ correctedImage ] = fun_undistort_depth_data(argDepthData, argRowCount
 	RESULT_PATH = "c:\tmp\cal\";
 	fprintf("\nBEGIN: fun_undistort_depth_data\n");
 
-	%depthFile = fullfile('d:\', 'work', 'sample_depth_data.txt'); %sprintf('rgb%d.png', i));
-	%depthFile = fullfile('sample_depth_data.txt'); %sprintf('rgb%d.png', i));
-	%argDepthData = importdata(depthFile);
-	[sz_rows, sz_cols] = size(argDepthData);
-	%fprintf("%s file is imported, sizes are %d %d \n", depthFile, sz_rows, sz_cols);
-	fprintf("depth data sizes are %d %d \n", sz_rows, sz_cols);
-
-	colIndex = 3;
-	seqDepthImage = zeros(argRowCount, argColCount);
-	for i = 1:argRowCount
-		for j = 1:argColCount
-			rowIndex = (i - 1) * argColCount + j;
-			%seqDepthImage(i, j) = 0;
-			%if (j == 20)
-			%    fprintf ("i %d, j %d, row %d depth %d \n", i, j, rowIndex, argDepthData(rowIndex, colIndex));
-			%end
-			seqDepthImage(i, j) = argDepthData(rowIndex, colIndex);
-		end
-	end
-
 	figure;
 	title('Original');
 	hold on;
-	imgResult = imshow(seqDepthImage, []);
+	imgResult = imshow(argDepthData, []);
 	hold off;
 	
 	figure;
