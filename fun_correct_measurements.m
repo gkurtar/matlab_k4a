f3c5b9b085c4b6fa42dd5376f5bd70b427480f74
gkurtar <gultekinkurtar@gmail.com>
2022-06-29 14:37:38 +0300

    Update fun_correct_measurements.m

--- fun_correct_measurements.m
+++ fun_correct_measurements.m
@@ -1,46 +1,50 @@
-
-
 %
-% Given a w x h x 3 vector of depth measurements where w and h are width and height of the
-% input image respectively and w x h x 3 vector of linear model objects where each model
+% fun_correct_measurements
+%
+% Given a 2D (w x h) array of depth measurements where w and h are width and height of the
+% input image respectively and a 2D (w x h) array of  linear model objects where each model
 % represents the mean value of the evaluated probability distribution at the corresponding
 % pixel this method returns the corrected measurements.
 %
 % Input Arguments:
-%    depthImage     -> Depth Data of x y z point triples, i.e. a (:, 3) matrix
-%    imageWidth     -> Depth Image Width
-%    imageHeight    -> Depth Image Height 
-%    vecLinearModels -> w x h x 3 vector of linear models
+%   argDepthImage            -> Depth Image Data of size (argHeight x argWidth)
+%   argWidth                 -> Depth Image Width
+%   argHeight                -> Depth Image Height 
+%   argMeanLinearModelMatrix -> 2D array of size (argHeight x argWidth) where each element
+%								is a linear model object of the corresponding pixel
 %
 % Output Values:
-%    correctedImage -> w x h x 3 vector of corrected depth measurements
+%   resCorrectedImage        -> Corrected Depth Image Data of size (argHeight x argWidth).
 %
-function [ correctedImage ] = fun_correct_measurements(depthImage, imageWidth, imageHeight, vecLinearModels)
+function [ resCorrectedImage ] = fun_correct_measurements(argDepthImage, argWidth, argHeight, argMeanLinearModelMatrix)
 
-	fprintf("\nBEGIN: fun_correct_measurements");
+	fprintf("\nBEGIN: fun_correct_measurements\n");
 
-	if (isempty(depthImage) || isempty(argLinearModels))
+	if (isempty(argDepthImage) || isempty(argLinearModels))
 		error('both input argument array sizes (%d and %d) should be gt zero',...
-			size(depthImage), size(argLinearModels));
-	elseif (isnan(str2double(imageWidth)) || isnan(str2double(imageHeight)))
-		error('width (%s) and height (%s) arguments must be an integer', imageWidth, imageHeight);
-	elseif ((size(depthImage, 1) ~= size(vecLinearModels, 1)) || (size(depthImage, 1) ~= imageHeight * imageWidth))
+			size(argDepthImage), size(argLinearModels));
+	elseif (isnan(str2double(argWidth)) || isnan(str2double(argHeight)))
+		error('width (%s) and height (%s) arguments must be an integer', argWidth, argHeight);
+	%{
+	elseif ((size(argDepthImage, 1) ~= size(argMeanLinearModelMatrix, 1)) || (size(argDepthImage, 1) ~= argHeight * argWidth))
 		fprintf('row sizes of input argument matrices (%d and %d) should be equal and match height argument (%d) ', ...
-            size(depthImage, 1), size(vecLinearModels, 1));
-	elseif ((size(depthImage, 2) ~= size(vecLinearModels, 2)) || (size(depthImage, 2) ~= 3))
+            size(argDepthImage, 1), size(argMeanLinearModelMatrix, 1));
+	elseif ((size(argDepthImage, 2) ~= size(argMeanLinearModelMatrix, 2)) || (size(argDepthImage, 2) ~= 3))
 		fprintf('column sizes of input argument matrices (%d and %d) should be equal and match width argument (%d) ', ...
-            size(depthImage, 1), size(vecLinearModels, 1));
+            size(argDepthImage, 1), size(argMeanLinearModelMatrix, 1));
+	%}
 	else
-		fprintf('going to correct depth data');
+		fprintf("going to correct depth data");
 	end
 	
-	correctedImage=zeros(imageHeight * imageWidth, 3);
+	resCorrectedImage=zeros(argHeight, argWidth);
 	
-	for i = 1 : imageHeight
-		for j = 1 : imageWidth
-			rowIndex = (i - 1) * imageWidth + j;
-			org_depth = depthImage(rowIndex, 3); %assign z val
-			mean_lm = vecLinearModels(rowIndex, 3); %find linear model
+	for i = 1 : argHeight
+		for j = 1 : argWidth
+			%{
+			rowIndex = (i - 1) * argWidth + j;
+			org_depth = argDepthImage(rowIndex, 3); %assign z val
+			mean_lm = argMeanLinearModelMatrix(rowIndex, 3); %find linear model
 			
 			if (org_depth == 0 || mean_lm == 0)
 				revised_depth = org_depth;
@@ -48,12 +52,24 @@ function [ correctedImage ] = fun_correct_measurements(depthImage, imageWidth, i
 				revised_depth = predict(mean_lm, org_depth);
 			end
 			
-			correctedImage(rowIndex, 1) = j;
-			correctedImage(rowIndex, 2) = i;
-			correctedImage(rowIndex, 3) = int32(revised_depth);	
+			resCorrectedImage(rowIndex, 1) = j;
+			resCorrectedImage(rowIndex, 2) = i;
+			resCorrectedImage(rowIndex, 3) = int32(revised_depth);
+			%}
+			
+			org_depth = argDepthImage(i, j); %assign z val
+			mean_lm = argMeanLinearModelMatrix(i, j); %find linear model
+			
+			if (org_depth == 0 || mean_lm == 0)
+				revised_depth = org_depth;
+			else
+				revised_depth = predict(mean_lm, org_depth);
+			end
+
+			resCorrectedImage(i, j) = int32(revised_depth);
 		end
 	end
 	
-	fprintf("\nEND: fun_correct_measurements");
+	fprintf("\nEND: fun_correct_measurements\n");
 	return;
 end
