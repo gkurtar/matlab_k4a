01b202ecbfede8af1b316ec833b069f5570f8eab
gkurtar <gultekinkurtar@gmail.com>
2022-07-21 20:22:56 +0300

    Update start.m

--- start.m
+++ start.m
@@ -36,10 +36,12 @@ fprintf("\nBEGIN: k4a depth camera calibration script\n");
 	
 	
 	prompt = {'Enter RGB calibration image square size (cm):',...
-		'Enter IR calibration image square size (cm):', 'Enter depth image distances (cm):'};
+				'Enter IR calibration image square size (cm):',...
+				'Enter depth image distances (cm):',...
+				'Enter depth data size:'};
 	dlgtitle = 'Please enter input parameters!';
-	dims = [1 45; 1 45; 1 45];
-	definput = {'25', '25', '50 100 150'};
+	dims = [1 45; 1 45; 1 45; 1 45];
+	definput = {'25', '25', '50 100 150', '480 640'};
 	answer = inputdlg(prompt, dlgtitle, dims, definput);
 	
 	rgb_sq_size	= str2num(answer{1});
@@ -51,90 +53,30 @@ fprintf("\nBEGIN: k4a depth camera calibration script\n");
 	seq_distances = str2num(answer{3});
 	disp(seq_distances);
 	
+	depth_data_matrix_size = str2num(answer{4});
+	disp(depth_data_matrix_size);
+	
 	fprintf("\nCalibration image square sizes are IR: %d and RGB: %d, ",...
 		ir_sq_size, rgb_sq_size);
-		
 	frmt=['distances: ' repmat(' %4d', 1, numel(seq_distances)) '\n'];
 	fprintf(frmt, seq_distances);
 
-	
-	if (3 < 5) return; end;
-	
-	%Read Average IR images and Average Depth Point Clouds from the specified folder and store in a table
-	%They are going to be used for probability based evaluation.
-	
-	%ir images going to be used for probability based evaluation
-	pbe_ir_files_dir = dir(fullfile(PBCAL_DATA_PATH, '*.png'));
-	%point clouds going to be used for probability based evaluation
-	pbe_point_cloud_files_dir = dir(fullfile(PBCAL_DATA_PATH, '*.txt'));
-	seq_pbe_ir_images = {}; %zeros(numel(pbe_ir_files_dir));
-	seq_pbe_point_clouds = {}; %zeros(numel(pbe_point_cloud_files_dir));
-	
-	fprintf("PBCAL_DATA_PATH %s, IR: %d, PC: %d, DIST: %d\n", PBCAL_DATA_PATH,  ...
-		numel(pbe_ir_files_dir), numel(pbe_point_cloud_files_dir), numel(distances));
-	
-
-	for i = 1 : numel(pbe_ir_files_dir)
-		filepath = fullfile(pbe_ir_files_dir(i).folder, pbe_ir_files_dir(i).name);
-		%seq_pbe_ir_images = [seq_pbe_ir_images, imread(filepath)];
-		seq_pbe_ir_images = [seq_pbe_ir_images; string(filepath)];
+	%Select point clouds for each distance from disk and store in the corresponding array
+	%%point clouds are going to be used for detecting parameters of probability based evaluation
+	seq_all_point_clouds = {};
+	for i = 1 : numel(seq_distances)
+		fprintf("\nDistances: %d , \n", seq_distances(i));	
+		strTitle = sprintf("Select multiple point cloud data files for %d cm distance!", seq_distances(i));
+		seq_point_clouds = fun_get_files(strTitle, 3);
+		fprintf("\nSelected %d point cloud files for distance %d cm\n", numel(seq_point_clouds), seq_distances(i));
+		disp(seq_point_clouds);
+		seq_all_point_clouds{i} = seq_point_clouds;
 	end
 	
+	fprintf("\nSelected %d point cloud files\n", numel(seq_all_point_clouds));
+	disp(seq_all_point_clouds);
 
-	
-	seq_avg_point_clouds = {};
-	seqPointClouds = {};
-	seqPointCloudsForSpecificDistance = {};
-	
-	%read in depth data
-	for i = 1 : numel(distances)
-		strMatchPattern = sprintf("*_%d.txt", distances(i));
-		point_cloud_files_dir = dir(fullfile(DEPTH_DATA_PATH, strMatchPattern));
-		fprintf("\nnum of matched files for %d : %d\n", distances(i), numel(point_cloud_files_dir));
-		seqPointClouds = {};
-		for j = 1 : numel(point_cloud_files_dir)
-			filepath = fullfile(point_cloud_files_dir(j).folder, point_cloud_files_dir(j).name);
-			
-			disp(filepath);
-			if (contains(point_cloud_files_dir(j).name, 'avg'))
-				seq_avg_point_clouds = [seq_avg_point_clouds; string(filepath)];
-				disp(strcat("found ", point_cloud_files_dir(j).name));
-			else
-				seqPointClouds = [seqPointClouds, string(filepath)];
-			end
-			disp("sdf #");
-			seqPointClouds
-			disp("sdf $");
-		end
-		
-		seqPointCloudsForSpecificDistance = [seqPointCloudsForSpecificDistance; seqPointClouds];
-		
-		seqDepthDataMatrixSizes = [seqDepthDataMatrixSizes; depthDataMatrixSize];
-	end
-	
-	%disp(seqDepthDataMatrixSize(1));
-	%datas = cell2struct(seqDepthDataMatrixSize, {'shortname', 'longname'}, 2);
-	%disp(datas);
-	
-	disp("\n-------\n");
-	disp(seqPointCloudsForSpecificDistance);
-	disp("=======\n");
-	
-	stcMeasurement.ranges = distances;
-	stcMeasurement.irFilePaths = seq_pbe_ir_images;
-	%stcMeasurement.pcFilePaths = seq_pbe_point_clouds;
-	stcMeasurement.pcAvgFilePaths = seq_avg_point_clouds;
-	stcMeasurement.pcFilePaths = seqPointCloudsForSpecificDistance;
-	%stcMeasurement.matsize = depthDataMatrixSize;%stcMeasurement.matsize = datas;
-	stcMeasurement.depthDataSizes = seqDepthDataMatrixSizes;
-	tblCalData = struct2table(stcMeasurement);
-
-	fprintf("\nRead Data for probability based evaluation into a table:\n");
-	disp(stcMeasurement);
-	fprintf("\nTable:\n");
-	disp(tblCalData);
-	
-	
-	fun_k4a_calibration(seq_rgb_images, 25, seq_ir_images, 25, tblCalData);
+	%fun_k4a_calibration(seq_rgb_images, rgb_sq_size, seq_ir_images,...
+	%	ir_sq_size, seq_distances, seq_all_point_clouds, depth_data_matrix_size);
 
 fprintf("\nEND: k4a depth camera calibration script\n");
